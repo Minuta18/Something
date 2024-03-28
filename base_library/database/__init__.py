47fd602594ce1668046a1bd66c5e08a3cf760b9a
Minuta18 <ig.samsonov10@yandex.ru>
2024-03-28 12:26:31 +0300

    Set upping db

---
+++ base_library/database/__init__.py
@@ -0,0 +1,1 @@
+from base_library.database.database import initialize_database
--- base_library/database/database.py
+++ base_library/database/database.py
@@ -1,4 +1,13 @@
 import sqlalchemy
+from sqlalchemy import ext
+from sqlalchemy import orm
 
 def initialize_database(database_url: str):
+    engine = ext.asyncio.create_async_engine(database_url)
+    session = orm.sessionmaker(
+        bind=engine,
+        class_=ext.asyncio.AsyncSession,
+        expire_on_commit=False,
+    )
     
+    return engine, session
